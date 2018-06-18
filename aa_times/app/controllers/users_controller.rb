6fac3cd58187dda789191ab7820125453b496a1b
Alex <a.plyushchay@gmail.com>
2018-06-18 14:42:47 -0400

    Being App Academy Times

---
+++ aa_times/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def user_params
+  end
+end
